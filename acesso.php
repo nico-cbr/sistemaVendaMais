3fb63ebab79f7a9038f15518a65ca9d81aa9828f
nico-cbr <ccarvalhonicoly@gmail.com>
2024-09-26 17:17:26 -0300

    atualizando e colocando funçao no login

--- acesso.php
+++ acesso.php
@@ -5,6 +5,21 @@
 // Recebendo dados da tela de login (front-end)
   $cpf = $_REQUEST['cpf'];
   $senha = $_REQUEST['senha'];
-  
+
   // echo "Olá seu cpf: $cpf e senha $senha";
+
+  // selecione o usuario que possui o cpf e a senha incluido no login
+  $sql = "SELECT * FROM usuario WHERE cpf='$cpf' AND senha='$senha' ";
+
+  // executa codigo sql com a permissao da conexao 
+  $resultado = mysqli_query($conexao, $sql);
+
+  // cada valor associdado ao nome da coluna no banco
+  $colunas = mysqli_fetch_assoc($resultado);
+
+  if (mysqli_num_rows($resultado) > 0) {
+    echo "Login efetuado com sucesso";
+  }else{
+    echo "Login não efetuado";
+  }
 ?>
