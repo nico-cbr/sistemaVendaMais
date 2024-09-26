65a63a2ea067b7b2a11c355e21a37e4437227fd4
nico-cbr <ccarvalhonicoly@gmail.com>
2024-09-26 16:38:39 -0300

    atualizando login

--- acesso.php
+++ acesso.php
@@ -1,7 +1,10 @@
 <?php
+// Importando arquivo de conexão com o banco
   include 'conexao.php';
 
+// Recebendo dados da tela de login (front-end)
   $cpf = $_REQUEST['cpf'];
   $senha = $_REQUEST['senha'];
-  echo "Olá seu cpf: $cpf e senha $senha";
+  
+  // echo "Olá seu cpf: $cpf e senha $senha";
 ?>
